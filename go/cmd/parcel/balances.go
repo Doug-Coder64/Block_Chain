af089b9b26abda541123c2794f85488b354d2d86
doug peterson <dougpeterson@dougs-MacBook-Pro.local>
2022-03-26 10:30:19 -0500

    a-cli

---
+++ go/cmd/parcel/balances.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+func balanceCmd() *cobra.Command {
+	var balancesCmd = &cobra.Command{
+		Use: "balances",
+		Short: "interact with balances (list...).",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return incorrectUsageErr()
+		},
+		Run: func(cmd *cobra.Command, args []string) {},
+	}
+
+	balancesCmd.AddCommand(balancesListCmd())
+
+	return balancesCmd
+}
+
+func balancesListCmd() *cobra.Command{
+	var balancesListCmd = &cobra.Command{
+	Use: "list", 
+	Short: "Lists all balances.",
+	Run: func(cmd *cobra.Command, args []string) {
+		state, err := database.NewStateFromDisk()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		defer state.Close()
+
+		fmt.Println("Acocunts Balances:")
+		fmt.Println("__________________")
+		fmt.Println("")
+		for account, balances := range state.Balances {
+			fmt.Println(fmt.Sprintf("%s: %d", account, balances))
+		}
+	},
+	}
+
+	return balancesListCmd
+}
